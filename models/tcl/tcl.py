7f1beaaa22e33002333c13c18c81ae647969f382
战颖 <liuqinying.lqy@antgroup.com>
2023-09-23 21:07:36 +0800

    cot_ce

--- models/tcl/tcl.py
+++ models/tcl/tcl.py
@@ -173,7 +173,7 @@ class Classification(nn.Module):
             self.w = init_w
             self.b = init_b
         
-        self.binary_cross_entropy_with_logits = nn.BCEWithLogitsLoss()
+        # self.binary_cross_entropy_with_logits = nn.BCEWithLogitsLoss()
         # self.tagging_loss_function = AsymmetricLoss(gamma_neg=7, gamma_pos=0, clip=0.05)
         # self.focalloss = FocalLoss(alpha=0.75, gamma=0.1)
 
@@ -190,12 +190,12 @@ class Classification(nn.Module):
         logits_per_img = logits_per_img * self.w + self.b
         # logits_per_img = torch.sigmoid(logits_per_img).mean(dim=1)
         # loss = F.binary_cross_entropy(logits_per_img, labels)
-        loss = self.binary_cross_entropy_with_logits(logits_per_img, labels) 
+        # loss = self.binary_cross_entropy_with_logits(logits_per_img, labels) 
         # loss = self.tagging_loss_function(logits_per_img, labels) 
         # loss = self.focalloss(logits_per_img, labels) 
-        # preds = logits_per_img.softmax(dim=-1)
-        # labels = F.normalize(labels, dim=-1, p=1)
-        # loss = -(preds.clamp(1e-8).log() * labels).sum(-1).mean()
+        preds = logits_per_img.softmax(dim=-1)
+        labels = F.normalize(labels, dim=-1, p=1)
+        loss = -(preds.clamp(1e-8).log() * labels).sum(-1).mean()
         # loss = F.cross_entropy(logits_per_img * logit_scale, labels)
         return loss
 
